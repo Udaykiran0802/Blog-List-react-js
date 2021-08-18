22b2b6275f265500a2527a60cd8c19f24b2c960e
UdayKiran0802 <udaykiran0802.com>
2021-08-18 15:34:03 +0000

    Blog-list

---
+++ src/components/UserInfo/index.js
@@ -0,0 +1,15 @@
+import './index.css'
+
+const UserInfo = () => (
+  <div className="image-container">
+    <img
+      className="image-edit"
+      src="https://assets.ccbp.in/frontend/react-js/profile-img.png"
+      alt="profile pic"
+    />
+    <h1 className="image-heading">Wade Warren</h1>
+    <p className="image-paragraph">Software developer at Uk</p>
+  </div>
+)
+
+export default UserInfo
